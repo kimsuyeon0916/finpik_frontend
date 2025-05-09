06542e748d050dd1951cde19aa52b89366308095
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-05-09 11:05:35 +0000

    chore: storybook 설정 및 chromatic 배포 워크플로우 추가 (#14)
    * chore: packages/fds 내 storybook 추가 (#13)
    
    * chore: packages/fds 내 chromatic 추가 (#13)
    
    * chore: chromatic deployment 워크플로우 추가 (#13)

---
+++ packages/fds/.storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from '@storybook/react'
+import '../src/theme/styles.css'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}
+
+export default preview
