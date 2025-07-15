dea54a2428c1290f2ad449cc0d0d68d976567cc9
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-07-15 20:47:16 +0000

    feat: Accordian 컴포넌트 추가 (#32)
    * chore: @radix-ui/react-accordion 추가 (#31)
    
    * feat: typography 유틸리티 클래스 변경 (#31)
    
    * feat: Accordian 컴포넌트 추가 (#31)
    
    * feat: Accordian 컴포넌트 스토리 추가 (#31)

---
+++ packages/fds/src/components/Accordian/Accordian.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import * as React from 'react'
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+import { Icon } from '@/components'
+
+import { cn } from '@/lib/utils'
+
+function Accordion({ ...props }: React.ComponentProps<typeof AccordionPrimitive.Root>) {
+  return <AccordionPrimitive.Root data-slot="accordion" {...props} />
+}
+
+function AccordionItem({
+  className,
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Item>) {
+  return (
+    <AccordionPrimitive.Item
+      data-slot="accordion-item"
+      className={cn('border-b border-gs-9', className)}
+      {...props}
+    />
+  )
+}
+
+function AccordionTrigger({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Trigger>) {
+  return (
+    <AccordionPrimitive.Header className="flex">
+      <AccordionPrimitive.Trigger
+        data-slot="accordion-trigger"
+        className={cn(
+          'flex-between-align flex-1 p-[20px] text-left b2 transition-all outline-none disabled:pointer-events-none [&[data-state=open]>span]:rotate-180',
+          className,
+        )}
+        {...props}
+      >
+        {children}
+        <span className="pointer-events-none shrink-0 translate-y-0.5 transition-transform duration-200">
+          <Icon name="arrow-down" />
+        </span>
+      </AccordionPrimitive.Trigger>
+    </AccordionPrimitive.Header>
+  )
+}
+
+function AccordionContent({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof AccordionPrimitive.Content>) {
+  return (
+    <AccordionPrimitive.Content
+      data-slot="accordion-content"
+      className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden"
+      {...props}
+    >
+      <div className={cn('px-[20px] py-[18px] bg-bg-2', className)}>{children}</div>
+    </AccordionPrimitive.Content>
+  )
+}
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
