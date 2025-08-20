2165b8c15f976e165136056dd83a474e66fb6a5d
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-08-20 11:32:24 +0000

    feat: 전체 기능 구현

--- apps/web/src/app/loan/results/page.tsx
+++ apps/web/src/app/loan/results/page.tsx
@@ -1,13 +1,20 @@
+import { redirect } from 'next/navigation'
 import { LoanResultsClientPage } from '../../../domains/loanResults/containers/LoanResultsClientPage'
 import { cookies } from 'next/headers'
 
 const LoanResults = async () => {
+  // const cookieStore = await cookies()
+  // const refreshToken = cookieStore.get('refreshToken')
+  // console.log(cookieStore)
+  // refreshToken && cookieStore.set('refreshToken', refreshToken.value)
   const cookieStore = await cookies()
-  const refreshToken = cookieStore.get('refreshToken')
-  console.log(cookieStore)
-  refreshToken && cookieStore.set('refreshToken', refreshToken.value)
+  const token = cookieStore.get('accessToken')?.value
 
-  return <LoanResultsClientPage />
+  if (token) {
+    return <LoanResultsClientPage />
+  } else {
+    redirect('/sign-in')
+  }
 }
 
 export default LoanResults
