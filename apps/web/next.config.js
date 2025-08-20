2e54725db0d19b9e131bf80905b33a1cb145b6a4
kimsuyeon0916 <kimsuyeon_0916@naver.com>
2025-08-20 11:16:47 +0000

    feat: 전체 기능 구현

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/', // 루트 URL로 접근 시
+        destination: '/loan/results', // /results로 리디렉트
+        permanent: false, // 302 임시 리다이렉트 (true면 308 permanent)
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
